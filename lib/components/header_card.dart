1a2c3cf1dc48cb5e1fd7e231b3197b0d28c03b71
questc0der <pour64moi@gmail.com>
2025-08-06 10:31:48 +0300

    feat: cards for header and progress

---
+++ lib/components/header_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HeaderCard extends StatelessWidget {
+  const HeaderCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                CircleAvatar(radius: 20),
+                SizedBox(width: 10),
+                Text("Name", style: TextStyle(fontSize: 18)),
+              ],
+            ),
+          ),
+
+          // Right icon button
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Text("0"),
+                SizedBox(width: 5),
+                const Icon(
+                  Icons.local_fire_department_sharp,
+                  color: Colors.orange,
+                ),
+                SizedBox(width: 10),
+                Text("FR"),
+                SizedBox(width: 5),
+                const Icon(Icons.calendar_view_day_rounded),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pareto_lingo/components/header_card.dart';
+import 'package:pareto_lingo/components/progress.dart';
 
 class HomeState extends ConsumerStatefulWidget {
   const HomeState({super.key});
@@ -11,6 +13,8 @@ class HomeState extends ConsumerStatefulWidget {
 class Home extends ConsumerState<HomeState> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("This is ParetoLingo")));
+    return Scaffold(
+      body: Column(children: [HeaderCard(), Expanded(child: Progress())]),
+    );
   }
 }
