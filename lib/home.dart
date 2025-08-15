5691294caef3d7d7f6d775310695d5b0d2af844a
questc0der <pour64moi@gmail.com>
2025-08-15 07:33:26 +0300

    refactor: bottom navigation bar added

--- lib/home.dart
+++ lib/home.dart
@@ -11,10 +11,32 @@ class HomeState extends ConsumerStatefulWidget {
 }
 
 class Home extends ConsumerState<HomeState> {
+  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: _buildBottomNavigationBar(),
       body: Column(children: [HeaderCard(), Expanded(child: Progress())]),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return NavigationBar(
+      destinations: [
+        NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+        NavigationDestination(icon: Icon(Icons.podcasts), label: "Podcast"),
+        NavigationDestination(
+          icon: Icon(Icons.video_library_rounded),
+          label: "Video",
+        ),
+        NavigationDestination(icon: Icon(Icons.settings), label: "Setting"),
+      ],
+      selectedIndex: currentPageIndex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
     );
   }
 }
